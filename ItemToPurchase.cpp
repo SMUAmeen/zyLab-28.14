c8b55af6fcba7624500ee4193d74b609a8db88bd
Dani5751 <167731363+Dani5751@users.noreply.github.com>
2024-04-22 13:38:42 -0500

    Update ItemToPurchase.cpp

--- ItemToPurchase.cpp
+++ ItemToPurchase.cpp
@@ -1,1 +1,29 @@
-/* Type your code here */
+#include <iosstream>
+#include <string>
+
+using namespace std;
+
+class ItemToPurchase{
+private:
+  string itemName;
+  string itemPrice;
+  int itemQuantity;
+
+public: 
+
+ItemToPurchase(string name = "none", string description = "none", int price = 0, int quantity = 0)[
+  itemName = name;
+  itemDescription = description;
+  itemPrice = price;
+  itemQuantity = quantity; 
+}
+
+void SetDescription(string description){
+  itemDescription = description; 
+}
+
+void PrintItemCost() const {
+  cout << itemName << " " << itemQuantity << " @ $" << itemPrice << " =$" << (itemQuantity * itemPrice) << endl;
+  }
+};
+
