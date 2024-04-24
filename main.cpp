2d3e7287f2594fd4445abc29efd73d3ad445fc5e
trumanb1233 <167351150+trumanb1233@users.noreply.github.com>
2024-04-24 08:27:18 -0500

    Update main.cpp
    did some of the main file

--- main.cpp
+++ main.cpp
@@ -5,17 +5,44 @@ using namespace std;
 #include "ShoppingCart.h"
 
 void PrintMenu() {
-   /* Type your code here */
-   
+   cout << "MENU" << endl << "a - Add item to cart" << endl << "d - Remove item from cart" << endl << "c - Change item quantity" << endl
+   << "i - Output items' descriptions" << endl << "o - Output shopping cart" << endl << "q - Quit" << endl;
 }
 
 void ExecuteMenu(char option, ShoppingCart& theCart) {
-   /* Type your code here */
+   switch(option){
+      case "a":
+      case "d":
+      case "c":
+      case "i":
+      case "o":
+      case "q":
+      
    
 }
 
 int main() {
-   /* Type your code here */
+   string name, date;
+   ShoppingCart cart1;
+   string option;
+
+   cout << "Enter customer's name:" << endl;
+   getLine(cin, name);
+   cout << "Enter today's date:" << endl;
+   getLine(cin, date);
+
+   cout << "Customer name: " << name << endl;
+   cout << "Today's date: " << date << endl;
+
+   cart1(name, date);
+
+   PrintMenu();
+   cout << "Choose an option:" << endl;
+   cin << option;
+   while (option != "q"){
+   
+      
+   
    
    return 0;
 }
