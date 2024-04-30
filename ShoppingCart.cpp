e9aaf5ea1c6e680f74a772bcd9a8aa72e22fdd15
SMUAmeen <159220726+SMUAmeen@users.noreply.github.com>
2024-04-30 18:03:50 -0500

    Update ShoppingCart.cpp

--- ShoppingCart.cpp
+++ ShoppingCart.cpp
@@ -1,21 +1,27 @@
 class ShoppingCart{
+
 ShoppingCart::ShoppingCart(){
  customerName = "none";
  customerDate = "January 1, 2016";
 }
+
 ShoppingCart::ShoppingCart(string name, string date){
  customerName = name;
  customerDate = date;
 }
-string GetCustomerName(){
+
+string GetCustomerName const(){
  return customerName;
 }
-string GetDate(){
+
+string GetDate() const{
  return customerDate;
 }
+
 void AddItem(ItemToPurchase item){
  cartItems.push_back(item);
 }
+
 void RemoveItem(string name){
    for (const auto& obj : cartItems) {
         if(obj.GetName() == name){
@@ -26,6 +32,7 @@ void RemoveItem(string name){
    }
    }
 }
+
 void ModifyItem(ItemToPurchase item){
  for (const auto& obj : cartItems) {
   if((obj.GetName() == item.GetName()) && (item.GetDescription() != "none") && (item.GetPrice() != 0 && item.GetQuantity() != 0)){
@@ -36,6 +43,7 @@ void ModifyItem(ItemToPurchase item){
   }
  }
 }
+
 int GetNumItemsInCart(){
         int numItems = 0;  
         for (const auto& item : cartItems) {
@@ -44,6 +52,7 @@ int GetNumItemsInCart(){
       
         return numItems;
 }
+
 int GetCostOfCart() {
         int totalCost = 0;
         
@@ -53,4 +62,24 @@ int GetCostOfCart() {
         
         return totalCost;
 }
+
+ void PrintTotal(){
+ cout << GetCustomerName() << "'s Shopping Cart" - " << GetDate() << endl;
+ cout << "Number of Item's: " << GetNumItemsInCart() << endl;
+ cout << endl
+ for (const auto& item : cartItems) {
+  cout << item.GetName() << " " << item.GetQuantity() << " @ $" << item.GetPrice() << " = $" << item.GetPrice() * item.GetQuantity() << endl;
+ }
+ cout << endl;
+ cout << "Total: $" << GetCostOfCart() << endl;
+ }
+
+ void PrintDescriptions(){
+  cout << GetCustomerName() << "'s Shopping Cart" - " << GetDate() << endl;
+  cout << endl;
+  cout << "Item Descriptions: " << endl;
+  for (const auto& item : cartItems) {
+   cout << item.GetName() << ": " << item.GetDescription() << endl;
+  }
+ } 
 };
