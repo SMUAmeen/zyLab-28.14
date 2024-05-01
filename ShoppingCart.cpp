149b44c5c8c2d453294a702e4eb897602669eeea
Dani5751 <167731363+Dani5751@users.noreply.github.com>
2024-04-30 22:31:58 -0500

    Update ShoppingCart.cpp
    Updated syntax and variables, in order to ensure the proper output was being produced when completing cart.

--- ShoppingCart.cpp
+++ ShoppingCart.cpp
@@ -10,60 +10,67 @@ ShoppingCart::ShoppingCart(string name, string date){
  customerDate = date;
 }
 
-string GetCustomerName const(){
+string ShoppingCart::GetCustomerName() const{
  return customerName;
 }
 
-string GetDate() const{
+string ShoppingCart::GetDate() const{
  return customerDate;
 }
 
-void AddItem(ItemToPurchase item){
+void ShoppingCart::AddItem(const ItemToPurchase& item){
  cartItems.push_back(item);
 }
 
-void RemoveItem(string name){
-   for (const auto& obj : cartItems) {
-        if(obj.GetName() == name){
-         cartItems.remove(obj);
-        }
-    else{
-     cout << "Item not found in cart. Nothing removed." << endl;
-   }
-   }
-}
+void ShoppingCart::RemoveItem(const string& itemName){
+    bool found = false;
+ int i;
+ for (i = 0; i < cartItems.size() && cartItems[i].GetName() != itemName; ++i);
+ if (i == cartItems.size()) cout << "Item not found in cart. Nothing removed. \n";
+ else cartItems.erase(cartItems.begin() + i);
 
-void ModifyItem(ItemToPurchase item){
- for (const auto& obj : cartItems) {
-  if((obj.GetName() == item.GetName()) && (item.GetDescription() != "none") && (item.GetPrice() != 0 && item.GetQuantity() != 0)){
-   obj = item;
-  }
-  else{
-   cout << "Item not found in cart. Nothing removed." << endl;
-  }
+void ShoppingCart::ModifyItem(const ItemToPurchase& item){
+ int i;
+ for (i = 0; i < cartItems.size() && cartItems[i].GetName() != itemName.GetName(); i++);
+ if(i == cartItems.size()) {
+  if(item.GetDescription() != "none") cartItems.at(i).SetDescription(item.GetDescription());
+  if (item.GetPrice !=0.0) cartItems.at(i).SetPrice(item.GetPrice());
+  if (item.GetQuantity() 1+ 0) cartItems.at(i).SetQuantity(item.GetQuantity());
  }
+ else cout << "Item not found in cart. Nothing modified.\n";
 }
 
-int GetNumItemsInCart(){
-        int numItems = 0;  
-        for (const auto& item : cartItems) {
-            numItems++;
+
+ 
+int ShoppingCart::GetNumItemsInCart(){
+        int totalItems = 0;  
+        for (ItemsToPurchase item : cartItems) {
+            totalItems += item.GetQuantity();
         }
       
-        return numItems;
+        return totalItems;
 }
 
-int GetCostOfCart() {
-        int totalCost = 0;
-        
-        for (const auto& item : cartItems) {
-            totalCost += item.GetPrice() * item.GetQuantity();
+int ShoppingCart::PrintTotal() const {
+        cout << "OUTPUT SHOPPING CART\n":
+        if (cartItems.empty()){
+        cout << customerName << "'s Shopping Cart -" << currentDate << endl;
+        cout << "Number of items: 0" << endl << endl;
+        cout << "Shopping Cart IS EMPTY" << endl << endl;
+}
+ else{
+     cout << customerName << "'s Shopping Cart -" << currentDate << endl;
+     cout << "Number of items: " << GetNumItemsInCart() << endl << endl;
+     for (
+          ItemToPurchase item : cartItems){
+          item.PrintItemCost();
         }
-        
-        return totalCost;
+         cout << endl;
+ }
+        cout << "Total: $" << GetCostOfCart() << endl;
 }
 
- void PrintTotal(){
+ void ShoppingCart:: PrintTotal(){
  cout << GetCustomerName() << "'s Shopping Cart" - " << GetDate() << endl;
  cout << "Number of Item's: " << GetNumItemsInCart() << endl;
  cout << endl
@@ -74,12 +81,10 @@ int GetCostOfCart() {
  cout << "Total: $" << GetCostOfCart() << endl;
  }
 
- void PrintDescriptions(){
+ void PrintDescriptions() const{
   cout << GetCustomerName() << "'s Shopping Cart" - " << GetDate() << endl;
-  cout << endl;
-  cout << "Item Descriptions: " << endl;
-  for (const auto& item : cartItems) {
-   cout << item.GetName() << ": " << item.GetDescription() << endl;
-  }
- } 
-};
+  cout << "Item Descriptions" << endl;
+  for (ItemToPurchase item :: cartItems) {
+   }
+   if (!cartItems.size()) cout << "SHOPPING CART IS EMPTY\n";
+ }
